9cc937d5a0d6ccbe96b58c99d67257b7fa4bf8e5
George <aleksandrovgn@gmail.com>
2017-02-18 15:43:21 +0300

    add Todo.jsx

---
+++ app/components/Todo.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Todo = React.createClass({
+    render: function () {
+        var {text} = this.props;
+        return (
+            <div>
+                {text}
+            </div>
+        );
+    }
+});
+module.exports = Todo;
