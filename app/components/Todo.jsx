bc07684a5e9243272640fcbc1ce0e674f004db21
George <aleksandrovgn@gmail.com>
2017-04-15 16:17:43 +0300

    styling todo; video 103

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -3,12 +3,17 @@ var React = require('react');
 var Todo = React.createClass({
     render: function () {
         var {id, completed, text} = this.props;
+        var todoClassName = completed ? 'todo todo-completed' : 'todo';
         return (
-            <div onClick={() => {
+            <div className={todoClassName} onClick={() => {
                 this.props.onToggle(id);
             }}>
-                <input type="checkbox" checked={completed}/>
-                {text}
+                <div>
+                    <input type="checkbox" checked={completed}/>
+                </div>
+                <div>
+                    <p>{text}</p>
+                </div>
             </div>
         );
     }
