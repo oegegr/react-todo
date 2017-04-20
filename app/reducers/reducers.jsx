57abb6d23413d2ff4150c675c39f34571142bd2e
Georgy Alexandrov <GAlexandrov@scph.mipt.ru>
2017-04-20 18:42:36 +0300

    add redux store; video 119

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,5 @@
+var uuid = require('uuid');
+
 export var searchTextReducer = (state = '', action) => {
     switch (action.type) {
         case 'SET_SEARCH_TEXT':
@@ -7,10 +9,36 @@ export var searchTextReducer = (state = '', action) => {
     }
 };
 
-export var showCompletedReducer = (state = false, action) {
+export var showCompletedReducer = (state = false, action) => {
     switch (action.type) {
         case 'TOGGLE_SHOW_COMPLETED':
             return !state;
+        default:
+            return state;
+    }
+};
+
+export var todosReducer = (state = [], action) => {
+    switch (action.type){
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: uuid(),
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        case 'TOGGLE_TODO':
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    var nextCompleted = !todo.completed;
+                    return {
+                        ...todo,
+                        completed: nextCompleted,
+                    };
+                }
+            });
         default:
             return state;
     }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -13,4 +13,29 @@ describe('Reducers', () => {
          expect(res).toEqual(action.searchText);
       });
    });
+   describe('todosReducer', () => {
+      it('Should add new todo', () => {
+         var action = {
+            type: 'ADD_TODO',
+            text: 'Walk the dog'
+         };
+         var res =  reducers.todosReducer(df([]), df(action));
+         expect(res.length).toEqual(1);
+         expect(res[0].text).toEqual(action.text);
+      })
+      it('Should toggle todo', () => {
+         var todos = [{
+            id: '123',
+            text: 'Something',
+            completed: true
+         }];
+         var action = {
+            type: 'TOGGLE_TODO',
+            id: '123'
+         };
+         var res = reducers.todosReducer(df(todos), df(action));
+         expect(res[0].completed).toEqual(false);
+
+      });
+   })
 })
